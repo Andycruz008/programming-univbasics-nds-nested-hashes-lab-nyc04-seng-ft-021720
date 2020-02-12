a3d2d4eba243df97a01532a64cceb1ce9c70ec93
concurrent-server-0630 <andycruz008@gmail.com>
2020-02-12 02:38:10 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -29,7 +29,7 @@ end
 
 def aoh_lookup(aoh, row, key)
   
- return aoh[0][:occupation]
+ return aoh[row][key]
   
 end
 
@@ -37,6 +37,6 @@ def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
   
-  aoh[1][:occupation] = "News Anchor"
+  aoh[row][key] = "News Anchor"
   return aoh
 end
